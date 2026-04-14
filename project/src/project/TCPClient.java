320135b5bf27feae2552241dd53081ed1ec24af9
Davyd Pinheiro Jacinto <davepinheiro530@outlook.com>
2026-04-13 21:38:01 -0300

    Refactor TCPClient to improve socket handling

--- project/src/project/TCPClient.java
+++ project/src/project/TCPClient.java
@@ -1,27 +1,106 @@
 package project;
 
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.Scanner;
 
 public class TCPClient{
     public static void main(String args[]){
         Socket s = null;
+        DataInputStream in;
+        DataOutputStream out;
+
+        String ipLocal = "10.11.111.29";
+        String ipRemoto = "10.11.111.27";
 
         try{
             int serverPort = 7896;
-            s = new Socket("10.11.111.1", serverPort);
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            s = new Socket(ipRemoto, serverPort);
+            in = new DataInputStream(s.getInputStream());
+            out = new DataOutputStream(s.getOutputStream());
 
             Scanner reader = new Scanner(System.in);
             String line;
 
             while(true){
-                line = reader.nextLine();
-                out.writeUTF(line);
-            }
+                int j = Integer.parseUnsignedInt(in.readUTF());
+                for(int i = 0; i < j; i++){
+                    String text = in.readUTF();
+                    System.out.println(text);
+                }
+                j = 0;
 
+                String lineCommand = in.readUTF();
+                
+                switch(lineCommand){
+                    case "end":
+                        line = reader.nextLine();
+                        out.writeUTF(line);
+                        break;
+                    case "end1":
+                        break;
+                    case "end2":
+                        line = reader.nextLine();
+                        out.writeUTF(line);
+
+                        try {
+                            ServerSocket serverSocket = new ServerSocket(7897);
+                            Socket client = serverSocket.accept();
+                            ObjectInputStream client_in = new ObjectInputStream(client.getInputStream());
+                            try{
+                                MeiosDeTransporte veiculo = (MeiosDeTransporte) client_in.readObject();
+                                System.out.println(veiculo.toString());
+                                client_in.close();
+                                
+                                try{
+                                    File arq = new File("serial");
+                                    arq.delete();
+                                    arq.createNewFile();
+                                    ObjectOutputStream objOutput = new ObjectOutputStream(new FileOutputStream(arq));
+
+                                    objOutput.writeObject(veiculo);
+                                    objOutput.flush();
+                                    objOutput.close();
+                                } catch(NullPointerException e){
+                                    System.out.println("NullPointerException: " + e.getLocalizedMessage());
+                                }
+                            } catch(ClassNotFoundException e){
+                                System.out.println("A classe não foi encontrada " + e.getMessage());
+                            }
+                            client.close();
+                            serverSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case "end3":
+                        try {
+                            ObjectInputStream objInput = new ObjectInputStream(new FileInputStream("serial"));
+                            MeiosDeTransporte v = (MeiosDeTransporte) objInput.readObject();
+
+                            Socket soc = new Socket(ipRemoto,7898);
+                            ObjectOutputStream objOutput = new ObjectOutputStream(soc.getOutputStream());
+
+                            System.out.println("Você está devolvendo:");
+                            System.out.println(v.toString());
+
+                            objOutput.writeObject(v);
+                            objOutput.flush();
+                            objOutput.close();
+                            objInput.close();
+                            soc.close();
+                        } catch (IOException e) {
+                            System.out.println("IOException: " + e.getMessage());
+                        } catch (ClassNotFoundException e){
+                            System.out.println("ClassNotFoundException: " + e.getMessage());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                lineCommand = "";
+            }
         } catch(UnknownHostException e){
             System.out.println("Socket: " + e.getMessage());
         } catch(EOFException e){
@@ -37,6 +116,5 @@ public class TCPClient{
                 }
             }
         }
-
     }
 }
